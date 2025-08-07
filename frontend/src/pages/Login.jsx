4eab59e982414df20144cf8b2b65c706d4ff8f66
Shital Gautam <shitalgautam34@gmail.com>
2025-08-07 08:57:36 +0545

    Add login/signup html

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import "../styles/auth.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-container">
+      <div className="intro">
+        <h1 className="intro-title">Welcome Back!</h1>
+        <h3 className="intro-sub_title">
+          Sign in to your account and continue your startup journey
+        </h3>
+      </div>
+      <form className="login-form">
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input type="email" required placeholder="Enter your email" />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Password</label>
+          <input type="password" required placeholder="Enter your password" />
+        </div>
+        <button className="signin">Sign In</button>
+        <div className="footer">
+          <p>Or continue with</p>
+          <button className="with_google">
+            <FaGoogle />
+            Google
+          </button>
+        </div>
+
+        <p>
+          Don't have an account?{" "}
+          <span>
+            <Link to="/signup">
+              Sign up
+              <FaArrowRight />
+            </Link>
+          </span>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "../styles/auth.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup-container">
+      <div className="intro">
+        <h1 className="intro-title">Join RateMyStartup</h1>
+        <h3 className="intro-sub_title">
+          Create your account and start sharing your startup ideas
+        </h3>
+      </div>
+      <form className="login-form">
+        <div className="field">
+          <label htmlFor="username">Username</label>
+          <input type="text" required placeholder="@Username" />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input type="email" required placeholder="Enter your email" />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Password</label>
+          <input type="password" required placeholder="Enter your password" />
+        </div>
+        <button className="signup">Create Account</button>
+        <div className="footer">
+          <p>Or continue with</p>
+          <button className="with_google">
+            <FaGoogle />
+            Google
+          </button>
+        </div>
+
+        <p>
+          Already have an account?{" "}
+          <span>
+            <Link to="/login">
+              Login
+              <FaArrowRight />
+            </Link>
+          </span>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
